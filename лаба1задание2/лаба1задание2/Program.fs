374773e0f9281123a294b38b393d8d981947a86f
zulenxd <artem.nazarenko06@gmail.com>
2026-03-04 08:57:22 +0500

    Add files via upload

---
+++ лаба1задание2/лаба1задание2/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let rec maxcifra n = 
+    if n = 0 then
+        0
+    else 
+        max (maxcifra(n/10)) (n%10)
+
+[<EntryPoint>]
+let main args = 
+    printf "введите число: "
+    let input = Console.ReadLine()
+    let n = abs (int input)
+    let cifra = maxcifra n
+    printf "%i" cifra
+    0
