7195ef2a35c576d11c06f4f74b9d9e974fb59b4a
zulenxd <artem.nazarenko06@gmail.com>
2026-06-16 22:07:31 +0500

    Update z3.fs

--- lab33/z3.fs
+++ lab33/z3.fs
@@ -1,7 +1,8 @@
-﻿// Задание 3. Вариант 4.
+// Задание 3. Вариант 4.
 // Вывести самое длинное название файла в указанном каталоге.
 
 open System
+open System.Text
 open System.IO // Обязательно подключаем эту библиотеку для работы с файлами и папками
 
 /// Безопасный ввод пути к каталогу (проверяем, существует ли он)
@@ -17,33 +18,31 @@ let rec readDirectory prompt =
 
 [<EntryPoint>]
 let main _ =
+
+    Console.OutputEncoding <- Encoding.UTF8
+    Console.InputEncoding <- Encoding.UTF8
+
     // 1. Запрашиваем путь к каталогу (например, C:\Windows или /usr/bin)
     let dirPath = readDirectory "Введите путь к каталогу: "
 
-    try
-        // 2. Получаем массив полных путей ко всем файлам в этой папке
-        let files = Directory.GetFiles(dirPath)
+    
+     // 2. Получаем массив полных путей ко всем файлам в этой папке
+    let files = Directory.GetFiles(dirPath)
 
-        // 3. Проверяем, не пуста ли папка
-        if files.Length = 0 then
-            printfn "\nВ указанном каталоге нет файлов."
-        else
-            // 4. Ищем самое длинное название файла
-            let longestFile =
-                files
-                // Оставляем только имена файлов (отрезаем путь вроде "C:\...")
-                |> Seq.map (fun f -> Path.GetFileName(f)) 
-                // Находим максимальный элемент по длине строки
-                |> Seq.maxBy (fun name -> String.length name)
+     // 3. Проверяем, не пуста ли папка
+    if files.Length = 0 then
+         printfn "\nВ указанном каталоге нет файлов."
+    else
+         // 4. Ищем самое длинное название файла
+         let longestFile =
+             files
+             // Оставляем только имена файлов (отрезаем путь вроде "C:\...")
+             |> Seq.map (fun f -> Path.GetFileName(f)) 
+             // Находим максимальный элемент по длине строки
+             |> Seq.maxBy (fun name -> String.length name)
 
-            printfn "\nСамое длинное название файла: \"%s\"" longestFile
-            printfn "Его длина: %d символов" (String.length longestFile)
+         printfn "\nСамое длинное название файла: \"%s\"" longestFile
+         printfn "Его длина: %d символов" (String.length longestFile)
             
-    with
-    // Блок обработки ошибок (на случай, если система не пустит нас в системную папку)
-    | :? UnauthorizedAccessException ->
-        printfn "\nОшибка: у программы нет прав доступа к этому каталогу."
-    | ex ->
-        printfn "\nПроизошла ошибка: %s" ex.Message
-
+  
     0
