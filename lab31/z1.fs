6f1c94dd8cdd0adfa267d345f09df1f13696c4f6
zulenxd <artem.nazarenko06@gmail.com>
2026-06-17 09:03:04 +0500

    Update z1.fs

--- lab31/z1.fs
+++ lab31/z1.fs
@@ -1,9 +1,5 @@
-﻿// Задание 1. Вариант 4.
-// Получить список из длин строк, содержащихся в исходном списке.
-
 open System
 
-/// Ввод целого положительного числа с повторением при ошибке
 let rec readInt prompt =
     printf "%s" prompt
     let input = Console.ReadLine()
@@ -13,7 +9,6 @@ let rec readInt prompt =
         printfn "Ошибка: введите положительное целое число."
         readInt prompt
 
-/// Запрашивает у пользователя заданное количество строк
 let readStrings count =
     seq {
         for i in 1 .. count do
@@ -21,22 +16,16 @@ let readStrings count =
             yield Console.ReadLine()
     }
 
-/// Получает длины строк из последовательности
 let getStringLengths strings =
-    // Используем Seq.map, чтобы преобразовать каждую строку (s) в её длину
     strings |> Seq.map (fun s -> String.length s)
 
 [<EntryPoint>]
 let main args =
     let count = readInt "Введите количество строк: "
     
-    // Получаем исходную последовательность строк
     let sequence = readStrings count
-    
-    // Получаем последовательность из длин этих строк
     let result = getStringLengths sequence
 
-    // Выводим результат, предварительно преобразовав последовательность в список
     printfn "Длины введенных строк: %A" (result |> Seq.toList)
     
     0
